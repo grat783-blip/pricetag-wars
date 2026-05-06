893efe61ee50db386bd980fde24e899d847bcda3
grat783-blip <grat783@gmail.com>
2026-05-06 09:12:57 -0500

    fill in real firebase config

--- firebase-config.js.js
+++ firebase-config.js.js
@@ -23,15 +23,12 @@
  *   hiding the API key. So it's safe to commit this file to git.
  * ============================================================ */
 
-window.PW_CONFIG = {
-  firebase: {
-    apiKey:            "AIzaSyDoAhW5wF2KUmT7-7w-TypOudfx2CDHtsw",
-    authDomain:        "pricetag-wars.firebaseapp.com",
-    databaseURL:       "https://pricetag-wars-default-rtdb.firebaseio.com",
-    projectId:         "pricetag-wars",
-    storageBucket:     "pricetag-wars.firebasestorage.app",
-    messagingSenderId: "71945708079",
-    appId:             "1:71945708079:web:40397719846ea370a25cda"
-  },
-  amazonTag: "pricetagwars-20"
+const firebaseConfig = {
+  apiKey: "AIzaSyDoAhW5wF2KUmT7-7w-TypOudfx2CDHtsw",
+  authDomain: "pricetag-wars.firebaseapp.com",
+  databaseURL: "https://pricetag-wars-default-rtdb.firebaseio.com",
+  projectId: "pricetag-wars",
+  storageBucket: "pricetag-wars.firebasestorage.app",
+  messagingSenderId: "71945708079",
+  appId: "1:71945708079:web:40397719846ea370a25cda"
 };
